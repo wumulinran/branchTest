0ea0da3057c51bb41bbcd25bd603560c99e9b99d
Ran <1156493358@qq.com>
2019-05-08 22:10:20 +0800

    第一次提交 master

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: Ran
+ * @Date: 2019/5/8
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
